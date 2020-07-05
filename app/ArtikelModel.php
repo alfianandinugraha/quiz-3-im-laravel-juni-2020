ccdb5dea98fbccda21c739b89547a06aec05b35e
Alfian Andi Nugraha <alfian.23andi@gmail.com>
2020-07-05 18:37:24 +0700

    menambahkan method-method untuk ArtikelModel

--- app/ArtikelModel.php
+++ app/ArtikelModel.php
@@ -4,7 +4,40 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 
+use Illuminate\Support\Facades\DB;
+
 class ArtikelModel extends Model
 {
-    //
+    public static function get_all()
+    {
+        $artikel = DB::table('artikel')->get();
+        return $artikel;
+    }
+
+    public static function create($data)
+    {
+        DB::table('artikel')->insert($data);
+    }
+
+    public static function get_once($id)
+    {
+        $artikel = DB::table('artikel')->where('id_artikel', '=', $id)->get();
+        return $artikel;
+    }
+
+    public static function delete($id)
+    {
+        DB::table('artikel')->where('id_artikel', '=', $id)->delete();
+    }
+
+    public static function edit($id, $data)
+    {
+        DB::table('artikel')->where('id_artikel', '=', $id)->update($data);
+    }
+
+    public static function get_by_slug($slug)
+    {
+        $artikel = DB::table('artikel')->where('slug', '=', $slug)->get();
+        return $artikel;
+    }
 }
